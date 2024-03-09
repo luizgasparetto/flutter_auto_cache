82f9ccd89e812f305b226a2b09bf73c6381d0a1b
zanelladev <arturf.zanella@gmail.com>
2024-03-09 00:50:03 -0300

    test: change adapters test

--- test/src/modules/cache/external/adapters/cache_adapter_test.dart
+++ test/src/modules/cache/external/adapters/cache_adapter_test.dart
@@ -6,15 +6,14 @@ import 'package:auto_cache_manager/src/modules/cache/external/adapters/enums/inv
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('CacheAdapter.fromJson |', () {
-    test('should be able to get CacheEntity from json successfully', () async {
-      // Arrange
-      const id = 'zanelladev';
-      const data = 'test';
-      const storageType = StorageType.kvs;
-      const invalidationType = InvalidationType.ttl;
-      final createAt = DateTime.utc(2024, 9, 9);
+  const id = 'zanelladev';
+  const data = 'test';
+  const storageType = StorageType.kvs;
+  const invalidationType = InvalidationType.ttl;
+  final createAt = DateTime.utc(2024, 9, 9);
 
+  group('CacheAdapter.fromJson |', () {
+    test('should be able to get CacheEntity from json successfully', () {
       final jsonCache = {
         'id': id,
         'data': data,
@@ -23,30 +22,57 @@ void main() {
         'created_at': createAt.toIso8601String(),
       };
 
-      // Act
       final cache = CacheAdapter.fromJson(
         jsonCache,
-        storageType: StorageType.kvs,
+        storageType: storageType,
       );
 
-      // // Assert
       expect(cache.id, equals(id));
       expect(cache.data, equals(data));
       expect(cache.storageType, equals(storageType));
       expect(cache.invalidationType, equals(invalidationType));
       expect(cache.createdAt, equals(createAt));
     });
+
+    test('should NOT be able to get CacheEntity from json when invalid values', () {
+      final jsonCache = {
+        'id': id,
+        'data': data,
+        'storage_type': storageType.name,
+        'invalidation_type': InvalidationTypeAdapter.toKey(invalidationType),
+        'created_at': 'error value @zanelladev',
+      };
+
+      expect(
+        () => CacheAdapter.fromJson(
+          jsonCache,
+          storageType: storageType,
+        ),
+        throwsFormatException,
+      );
+    });
+
+    test('should NOT be able to get CacheEntity from json when invalid keys', () {
+      final jsonCache = {
+        'invalidkey': id,
+        'data': data,
+        'storage_type': storageType.name,
+        'invalidation_type': InvalidationTypeAdapter.toKey(invalidationType),
+        'created_at': createAt.toIso8601String(),
+      };
+
+      expect(
+        () => CacheAdapter.fromJson(
+          jsonCache,
+          storageType: storageType,
+        ),
+        throwsA(isA<TypeError>()),
+      );
+    });
   });
 
   group('CacheAdapter.toJson |', () {
-    test('should be able to parse CacheEntity to json successfully', () async {
-      // Arrange
-      const id = 'zanelladev';
-      const data = 'test';
-      const storageType = StorageType.kvs;
-      const invalidationType = InvalidationType.ttl;
-      final createAt = DateTime.utc(2024, 9, 9);
-
+    test('should be able to parse CacheEntity to json successfully', () {
       final cache = CacheEntity(
         id: id,
         data: data,
@@ -55,14 +81,20 @@ void main() {
         createdAt: createAt,
       );
 
-      // Act
       final jsonCache = CacheAdapter.toJson(cache);
 
-      // Assert
+      expect(jsonCache.containsKey('id'), isTrue);
       expect(jsonCache['id'], equals(id));
+
+      expect(jsonCache.containsKey('data'), isTrue);
       expect(jsonCache['data'], equals(data));
-      expect(jsonCache['storage_type'], isNull);
+
+      expect(jsonCache.containsKey('storage_type'), isFalse);
+
+      expect(jsonCache.containsKey('invalidation_type'), isTrue);
       expect(jsonCache['invalidation_type'], equals(InvalidationTypeAdapter.toKey(invalidationType)));
+
+      expect(jsonCache.containsKey('created_at'), isTrue);
       expect(jsonCache['created_at'], equals(createAt.toIso8601String()));
     });
   });
--- test/src/modules/cache/external/adapters/enums/invalidation_type_adapter_test.dart
+++ test/src/modules/cache/external/adapters/enums/invalidation_type_adapter_test.dart
@@ -4,43 +4,34 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('InvalidationTypeAdapter.fromKey |', () {
-    test('should be able to get InvalidationType from key successfully', () async {
-      // Arrange
-      const refreshKey = 'refresh';
-      const ttlKey = 'ttl';
+    test('should be able to get InvalidationType from key successfully', () {
+      final refreshKey = InvalidationType.refresh.name;
+      final ttlKey = InvalidationType.ttl.name;
 
-      // Act
       final refresh = InvalidationTypeAdapter.fromKey(refreshKey);
       final ttl = InvalidationTypeAdapter.fromKey(ttlKey);
 
-      // Assert
       expect(refresh, equals(InvalidationType.refresh));
       expect(ttl, equals(InvalidationType.ttl));
     });
 
-    test('should be able to get InvalidationType.refresh from invalid key successfully', () async {
-      // Arrange
+    test('should be able to get InvalidationType.refresh from invalid key successfully', () {
       const invalidKey = 'invalid';
 
-      // Act
       final refresh = InvalidationTypeAdapter.fromKey(invalidKey);
 
-      // Assert
       expect(refresh, equals(InvalidationType.refresh));
     });
   });
 
   group('InvalidationTypeAdapter.toKey |', () {
-    test('should be able to get key from InvalidationType successfully', () async {
-      // Arrange
-      const expectedRefreshKey = 'refresh';
-      const expectedTtlKey = 'ttl';
+    test('should be able to get key from InvalidationType successfully', () {
+      final expectedRefreshKey = InvalidationType.refresh.name;
+      final expectedTtlKey = InvalidationType.ttl.name;
 
-      // Act
       final refreshKey = InvalidationTypeAdapter.toKey(InvalidationType.refresh);
       final ttlKey = InvalidationTypeAdapter.toKey(InvalidationType.ttl);
 
-      // Assert
       expect(refreshKey, equals(expectedRefreshKey));
       expect(ttlKey, equals(expectedTtlKey));
     });
