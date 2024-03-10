cfcb4ee6683cc4984e6274043f2519f8bd929240
Luiz Gasparetto <luizegasp@gmail.com>
2024-03-10 16:45:04 -0300

    ci: implemented java's distribution for CI

--- test/src/modules/cache/domain/usecases/get_cache_usecase_test.dart
+++ test/src/modules/cache/domain/usecases/get_cache_usecase_test.dart
@@ -14,7 +14,7 @@ class InvalidationCacheContextMock extends Mock implements InvalidationCacheCont
 
 class FakeAutoCacheManagerException extends Fake implements AutoCacheManagerException {}
 
-class CacheEntityFake extends Fake implements CacheEntity {}
+class CacheEntityFake<T extends Object> extends Fake implements CacheEntity<T> {}
 
 void main() {
   final repository = CacheRepositoryMock();
@@ -22,7 +22,7 @@ void main() {
 
   final sut = GetCache(repository, invalidationContext);
 
-  final cacheFake = CacheEntityFake();
+  final cacheFake = CacheEntityFake<String>();
 
   setUp(() {
     registerFallbackValue(cacheFake);
@@ -68,7 +68,7 @@ void main() {
       expect(response.isSuccess, isTrue);
       expect(response.success, isNull);
       verify(() => repository.findByKey<String>('my_key')).called(1);
-      verifyNever(() => invalidationContext.execute<String>(any()));
+      verifyNever(() => invalidationContext.execute<String>(any<CacheEntity<String>>()));
     });
 
     test('should NOT be able to get data in cache when findByKey retrives an exception', () async {
@@ -81,7 +81,7 @@ void main() {
       expect(response.isError, isTrue);
       expect(response.error, isA<AutoCacheManagerException>());
       verify(() => repository.findByKey<String>('my_key')).called(1);
-      verifyNever(() => invalidationContext.execute<String>(any()));
+      verifyNever(() => invalidationContext.execute<String>(any<CacheEntity<String>>()));
     });
 
     test('should NOT be able to get data in cache when invalidation context retrives an exception', () async {
