b255ec19901f36701827f512d000db549c1bef9b
zanelladev <arturf.zanella@gmail.com>
2024-03-22 09:47:09 -0300

    feat: cache config

--- lib/src/core/config/cache_config.dart
+++ lib/src/core/config/cache_config.dart
@@ -1,3 +1,5 @@
+import 'package:auto_cache_manager/src/modules/cache/domain/value_objects/cache_cryptography_options.dart';
+
 import '../../modules/cache/domain/constants/cache_constants.dart';
 import '../../modules/cache/domain/enums/invalidation_type.dart';
 import '../../modules/cache/domain/enums/storage_type.dart';
@@ -7,11 +9,13 @@ class CacheConfig {
   final StorageType storageType;
   final InvalidationType invalidationType;
   final CacheSizeOptions sizeOptions;
+  final CacheCryptographyOptions? cryptographyOptions;
 
   CacheConfig({
     this.storageType = CacheConstants.defaultStorageType,
     this.invalidationType = CacheConstants.defaultInvalidationType,
     CacheSizeOptions? sizeOptions,
+    this.cryptographyOptions,
   }) : sizeOptions = sizeOptions ?? CacheSizeOptions.createDefault();
 
   factory CacheConfig.defaultConfig() => CacheConfig();
@@ -24,4 +28,6 @@ class CacheConfig {
   }
 
   bool get isKvsSelected => this.storageType == StorageType.kvs;
+
+  bool get isCryptoActive => cryptographyOptions != null;
 }
