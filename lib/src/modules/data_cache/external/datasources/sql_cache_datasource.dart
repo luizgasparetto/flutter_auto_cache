e351b53f4fc8b0636e732f8f7364f81b83fc5ef4
Luiz Gasparetto <luizegasp@gmail.com>
2024-04-07 01:27:24 -0300

    chore: remove todo comments

--- lib/src/modules/data_cache/external/datasources/sql_cache_datasource.dart
+++ lib/src/modules/data_cache/external/datasources/sql_cache_datasource.dart
@@ -7,19 +7,16 @@ import '../../infra/datasources/i_sql_cache_datasource.dart';
 final class SQLCacheDatasource implements ISQLCacheDatasource {
   @override
   Future<void> clear() {
-    // TODO: implement clear
     throw UnimplementedError();
   }
 
   @override
   Future<CacheEntity<T>?> findByKey<T extends Object>(String key) {
-    // TODO: implement findByKey
     throw UnimplementedError();
   }
 
   @override
   Future<void> save<T extends Object>(SaveCacheDTO<T> dto) {
-    // TODO: implement save
     throw UnimplementedError();
   }
 }
