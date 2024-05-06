9ad06f636e801980e683f0dc27c8d1eef3286402
luizgasp <luizegasp@gmail.com>
2024-05-06 20:45:47 -0300

    feat: add response protocol

---
+++ lib/src/core/protocols/cache_response.dart
@@ -0,0 +1,23 @@
+import 'enums/cache_response_status.dart';
+
+abstract class CacheResponse<T> {
+  final T data;
+  final String status;
+
+  const CacheResponse({
+    required this.data,
+    required this.status,
+  });
+}
+
+class SuccessCacheResponse<T extends Object> extends CacheResponse<T> {
+  SuccessCacheResponse({required super.data}) : super(status: CacheResponseStatus.success.status);
+}
+
+class NotFoundCacheResponse extends CacheResponse<Null> {
+  NotFoundCacheResponse() : super(data: null, status: CacheResponseStatus.notFound.status);
+}
+
+class ExpiredCacheResponse extends CacheResponse<Null> {
+  ExpiredCacheResponse() : super(data: null, status: CacheResponseStatus.expired.status);
+}
---
+++ lib/src/modules/data_cache/infra/datasources/i_data_cache_datasource.dart
@@ -0,0 +1,37 @@
+import '../../domain/dtos/save_cache_dto.dart';
+import '../../domain/entities/cache_entity.dart';
+
+abstract interface class ICommandDataCacheDatasource {
+  /// Persists a data entity in the cache based on the details provided within a data transfer object (DTO).
+  ///
+  /// This asynchronous operation saves an entity to the cache using a key-value pair encapsulated within
+  /// the provided DTO. It ensures data integrity and type safety during the save operation.
+  ///
+  /// Parameters:
+  ///   - [dto]: A `SaveCacheDTO<T>` object containing the key under which the entity is stored and the entity itself.
+  ///
+  /// Returns:
+  ///   - A `Future<void>` that completes when the operation has successfully persisted the entity.
+  Future<void> save<T extends Object>(SaveCacheDTO<T> dto);
+
+  /// Deletes a specific cache entry identified by its key.
+  ///
+  /// This asynchronous operation removes a cache entry corresponding to the specified key. It is typically used
+  /// to manage cache size or remove outdated or unnecessary data.
+  ///
+  /// Parameters:
+  ///   - [key]: A `String` representing the unique key associated with the cache entry to be deleted.
+  ///
+  /// Returns:
+  ///   - A `Future<void>` that completes when the entry has been successfully removed.
+  Future<void> delete(String key);
+
+  /// Clears all entries from the cache.
+  ///
+  /// This comprehensive asynchronous operation is used to completely empty the cache, effectively resetting it.
+  /// It is useful during scenarios such as application resets, user logout, or when transitioning between major updates.
+  ///
+  /// Returns:
+  ///   - A `Future<void>` that completes when the cache has been entirely cleared.
+  Future<void> clear();
+}
