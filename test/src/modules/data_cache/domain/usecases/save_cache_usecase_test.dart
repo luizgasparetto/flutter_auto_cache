1ccc14786644c4b3a9b8753ddfe3b3bf0449bf0d
Luiz Gasparetto <luizegasp@gmail.com>
2024-05-07 16:19:25 -0300

    refactor: divide command & query datasources

--- test/src/modules/data_cache/domain/usecases/save_cache_usecase_test.dart
+++ test/src/modules/data_cache/domain/usecases/save_cache_usecase_test.dart
@@ -71,7 +71,7 @@ void main() {
     );
 
     test('should be able to save cache data successfully when not find any previous cache with same key', () async {
-      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenAnswer((_) async => right(null));
+      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenReturn(right(null));
       when(() => repository.save<String>(dto)).thenAnswer((_) async => right(unit));
 
       final response = await sut.execute<String>(dto);
@@ -83,8 +83,8 @@ void main() {
     });
 
     test('should NOT be able to save cache repository when findByKey fails', () async {
-      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenAnswer(
-        (_) async => left(AutoCacheManagerExceptionFake()),
+      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenReturn(
+        left(AutoCacheManagerExceptionFake()),
       );
 
       final response = await sut.execute<String>(dto);
@@ -97,11 +97,8 @@ void main() {
     });
 
     test('should NOT be able to save cache repository when InvalidationCacheContext fails', () async {
-      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenAnswer((_) async => right(fakeCache));
-
-      when(() => invalidationContext.execute<String>(fakeCache)).thenAnswer((_) {
-        return left(AutoCacheManagerExceptionFake());
-      });
+      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenReturn(right(fakeCache));
+      when(() => invalidationContext.execute<String>(fakeCache)).thenReturn(left(AutoCacheManagerExceptionFake()));
 
       final response = await sut.execute<String>(dto);
 
@@ -113,12 +110,9 @@ void main() {
     });
 
     test('should NOT be able to save cache repository when save method fails', () async {
-      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenAnswer((_) async => right(fakeCache));
+      when(() => repository.get<String>(any(that: cacheDtoMatcher()))).thenReturn(right(fakeCache));
       when(() => invalidationContext.execute<String>(fakeCache)).thenAnswer((_) => right(unit));
-
-      when(() => repository.save<String>(dto)).thenAnswer((_) async {
-        return left(AutoCacheManagerExceptionFake());
-      });
+      when(() => repository.save<String>(dto)).thenAnswer((_) async => left(AutoCacheManagerExceptionFake()));
 
       final response = await sut.execute<String>(dto);
 
---
+++ test/src/modules/data_cache/external/datasources/command_data_cache_datasource_test.dart
@@ -0,0 +1,85 @@
+import 'package:auto_cache_manager/src/core/core.dart';
+import 'package:auto_cache_manager/src/core/services/cryptography/i_cryptography_service.dart';
+import 'package:auto_cache_manager/src/core/services/storages/prefs/i_prefs_service.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/domain/dtos/save_cache_dto.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/domain/enums/storage_type.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/external/datasources/command_data_cache_datasource.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../../commons/extensions/when_extensions.dart';
+
+class PrefsServiceMock extends Mock implements IPrefsService {}
+
+class CryptographyServiceMock extends Mock implements ICryptographyService {}
+
+class FakeAutoCacheManagerException extends Fake implements AutoCacheManagerException {}
+
+void main() {
+  final prefsService = PrefsServiceMock();
+  final cryptographyService = CryptographyServiceMock();
+
+  final sut = CommandDataCacheDatasource(prefsService, cryptographyService);
+
+  tearDown(() {
+    reset(prefsService);
+    reset(cryptographyService);
+  });
+
+  group('CommandDataCacheDatasource.save |', () {
+    final config = CacheConfig.defaultConfig();
+    final dto = SaveCacheDTO<String>(key: 'my_key', data: 'data', storageType: StorageType.prefs, cacheConfig: config);
+
+    Matcher matcher() {
+      return predicate<String>((json) => json.contains('data') && json.contains('my_key'));
+    }
+
+    test('should be able to save data in prefs cache succesfully', () async {
+      when(() => cryptographyService.encrypt(any(that: matcher()))).thenReturn('any_encrypted');
+      when(() => prefsService.save(key: 'my_key', data: 'any_encrypted')).thenAsyncVoid();
+
+      await expectLater(sut.save(dto), completes);
+      verify(() => prefsService.save(key: 'my_key', data: 'any_encrypted')).called(1);
+    });
+
+    test('should NOT be able to save data in prefs when service fails', () async {
+      when(() => cryptographyService.encrypt(any(that: matcher()))).thenReturn('any_encrypted');
+      when(() => prefsService.save(key: 'my_key', data: 'any_encrypted')).thenThrow(FakeAutoCacheManagerException());
+
+      expect(() => sut.save(dto), throwsA(isA<AutoCacheManagerException>()));
+      verify(() => prefsService.save(key: 'my_key', data: 'any_encrypted')).called(1);
+    });
+  });
+
+  group('CommandDataCacheDatasource.delete |', () {
+    test('should be able to delete data in prefs cache successfully', () async {
+      when(() => prefsService.delete(key: 'my_key')).thenAsyncVoid();
+
+      await expectLater(sut.delete('my_key'), completes);
+      verify(() => prefsService.delete(key: 'my_key')).called(1);
+    });
+
+    test('should NOT be able to delete data in prefs cache when service fails', () async {
+      when(() => prefsService.delete(key: 'my_key')).thenThrow(FakeAutoCacheManagerException());
+
+      expect(() => sut.delete('my_key'), throwsA(isA<AutoCacheManagerException>()));
+      verify(() => prefsService.delete(key: 'my_key')).called(1);
+    });
+  });
+
+  group('CommandDataCacheDatasource.clear |', () {
+    test('should be able to clear prefs cache data successfully', () async {
+      when(prefsService.clear).thenAsyncVoid();
+
+      await expectLater(sut.clear(), completes);
+      verify(prefsService.clear).called(1);
+    });
+
+    test('should NOT be able to clear prefs when service fails', () async {
+      when(prefsService.clear).thenThrow(FakeAutoCacheManagerException());
+
+      expect(sut.clear, throwsA(isA<AutoCacheManagerException>()));
+      verify(prefsService.clear).called(1);
+    });
+  });
+}
