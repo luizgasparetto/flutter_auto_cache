7664d7246031a37b6b199f4e60bede852271a919
luizgasp <luizegasp@gmail.com>
2024-06-21 19:38:20 -0300

    test: create ttl invalidate unit tests

---
+++ test/src/modules/data_cache/domain/services/invalidation_service/strategies/ttl_invalidation_cache_strategy_test.dart
@@ -0,0 +1,38 @@
+import 'package:auto_cache_manager/src/core/core.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/domain/entities/cache_entity.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/domain/failures/invalidation_methods_failures.dart';
+import 'package:auto_cache_manager/src/modules/data_cache/domain/services/invalidation_service/strategies/ttl_invalidation_cache_strategy.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FakeNonExpiredCacheEntity extends Fake implements CacheEntity<String> {
+  @override
+  DateTime get endAt => DateTime.now().add(const Duration(days: 1));
+}
+
+class FakeExpiredCacheEntity extends Fake implements CacheEntity<String> {
+  @override
+  DateTime get endAt => DateTime.now().subtract(const Duration(days: 1));
+}
+
+void main() {
+  final sut = TTLInvalidationCacheStrategy();
+
+  final nonExpiredCacheEntity = FakeNonExpiredCacheEntity();
+  final expiredCachEntity = FakeExpiredCacheEntity();
+
+  group('TTLInvalidationCacheStrategy.validate |', () {
+    test('should be able to validate an not expired cache successfully', () {
+      final response = sut.validate(nonExpiredCacheEntity);
+
+      expect(response.isSuccess, isTrue);
+      expect(response.success, isA<Unit>());
+    });
+
+    test('should be able to validate an expired cache when endAt is before now', () {
+      final response = sut.validate(expiredCachEntity);
+
+      expect(response.isError, isTrue);
+      expect(response.error, isA<ExpiredTTLFailure>());
+    });
+  });
+}
