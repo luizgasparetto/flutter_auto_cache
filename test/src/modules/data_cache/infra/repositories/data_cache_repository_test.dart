660bfb0d986301a4e884eb3387303a7b275ecf4d
Luiz Gasparetto <luizegasp@gmail.com>
2024-06-29 19:55:07 -0300

    test(data_cache): create new unit test for infra methods

--- test/src/modules/data_cache/infra/repositories/data_cache_repository_test.dart
+++ test/src/modules/data_cache/infra/repositories/data_cache_repository_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_auto_cache/src/core/core.dart';
 import 'package:flutter_auto_cache/src/modules/data_cache/domain/dtos/delete_cache_dto.dart';
 import 'package:flutter_auto_cache/src/modules/data_cache/domain/dtos/get_cache_dto.dart';
+import 'package:flutter_auto_cache/src/modules/data_cache/domain/dtos/update_cache_dto.dart';
 import 'package:flutter_auto_cache/src/modules/data_cache/domain/dtos/write_cache_dto.dart';
 import 'package:flutter_auto_cache/src/modules/data_cache/domain/entities/data_cache_entity.dart';
 import 'package:flutter_auto_cache/src/modules/data_cache/infra/datasources/i_data_cache_datasource.dart';
@@ -38,7 +39,7 @@ void main() {
   group('DataCacheRepository.get |', () {
     const prefsDto = GetCacheDTO(key: 'my_key');
 
-    test('should be able to find cache data by key in prefs successfully', () async {
+    test('should be able to find cache data by key in prefs successfully', () {
       when(() => datasource.get<String>('my_key')).thenReturn(DataCacheEntityFake<String>(fakeData: 'any_data'));
 
       final response = sut.get<String>(prefsDto);
@@ -48,7 +49,7 @@ void main() {
       verify(() => datasource.get<String>('my_key')).called(1);
     });
 
-    test('should be able to return NULL when cache not found data in prefs', () async {
+    test('should be able to return NULL when cache not found data in prefs', () {
       when(() => datasource.get<String>('my_key')).thenReturn(null);
 
       final response = sut.get<String>(prefsDto);
@@ -58,7 +59,7 @@ void main() {
       verify(() => datasource.get<String>('my_key')).called(1);
     });
 
-    test('should NOT be able to get cache in prefs when datasource throws an AutoCacheManagerException', () async {
+    test('should NOT be able to get cache in prefs when datasource throws an AutoCacheException', () {
       when(() => datasource.get<String>('my_key')).thenThrow(FakeAutoCacheException());
 
       final response = sut.get<String>(prefsDto);
@@ -69,8 +70,67 @@ void main() {
     });
   });
 
+  group('DataCacheRepository.getList |', () {
+    final dataCache = DataCacheEntityFake<List<String>>(fakeData: ['data', 'data']);
+    const dto = GetCacheDTO(key: 'list_key');
+
+    test('should be able to get a list of string in prefs successfully', () {
+      when(() => datasource.getList<List<String>, String>('list_key')).thenReturn(dataCache);
+
+      final response = sut.getList<List<String>, String>(dto);
+
+      expect(response.isSuccess, isTrue);
+      expect(response.fold((l) => l, (r) => r), equals(dataCache));
+      verify(() => datasource.getList<List<String>, String>('list_key')).called(1);
+    });
+
+    test('should be able to get NULL when cache not found list data in prefs', () {
+      when(() => datasource.getList<List<String>, String>('list_key')).thenReturn(null);
+
+      final response = sut.getList<List<String>, String>(dto);
+
+      expect(response.isSuccess, isTrue);
+      expect(response.fold((l) => l, (r) => r), isNull);
+      verify(() => datasource.getList<List<String>, String>('list_key')).called(1);
+    });
+
+    test('should NOT be able to get cache in prefs when datasource throws an AutoCacheException', () {
+      when(() => datasource.getList<List<String>, String>('list_key')).thenThrow(FakeAutoCacheException());
+
+      final response = sut.getList<List<String>, String>(dto);
+
+      expect(response.isError, isTrue);
+      expect(response.fold((l) => l, (r) => r), isA<AutoCacheException>());
+      verify(() => datasource.getList<List<String>, String>('list_key')).called(1);
+    });
+  });
+
+  group('DataCacheRepository.getEncryptedData |', () {
+    final cache = DataCacheEntityFake<String>(fakeData: 'fake_data');
+
+    test('should be able to get encrypted data successfully', () {
+      when(() => datasource.getEncryptData<String>(cache)).thenReturn('encrypted_data');
+
+      final response = sut.getEncryptedData<String>(cache);
+
+      expect(response.isSuccess, isTrue);
+      expect(response.fold((l) => l, (r) => r), equals('encrypted_data'));
+      verify(() => datasource.getEncryptData<String>(cache)).called(1);
+    });
+
+    test('should NOT be able to get encrypted data when datasource fails', () {
+      when(() => datasource.getEncryptData<String>(cache)).thenThrow(FakeAutoCacheException());
+
+      final response = sut.getEncryptedData<String>(cache);
+
+      expect(response.isError, isTrue);
+      expect(response.fold((l) => l, (r) => r), isA<AutoCacheException>());
+      verify(() => datasource.getEncryptData<String>(cache)).called(1);
+    });
+  });
+
   group('DataCacheRepository.getKeys |', () {
-    test('should be able to get keys of prefs datasource successfully', () async {
+    test('should be able to get keys of prefs datasource successfully', () {
       when(() => datasource.getKeys()).thenReturn(['keys']);
 
       final response = sut.getKeys();
@@ -80,7 +140,7 @@ void main() {
       verify(() => datasource.getKeys()).called(1);
     });
 
-    test('should NOT be able to get keys of prefs when prefs datasource fails', () async {
+    test('should NOT be able to get keys of prefs when prefs datasource fails', () {
       when(() => datasource.getKeys()).thenThrow(FakeAutoCacheException());
 
       final response = sut.getKeys();
@@ -111,6 +171,30 @@ void main() {
       expect(response.isError, isTrue);
       expect(response.error, isA<AutoCacheException>());
       verify(() => datasource.save<String>(prefsDto)).called(1);
+    });
+  });
+
+  group('DataCacheRepository.update |', () {
+    final dto = UpdateCacheDTO(previewCache: DataCacheEntityFake(fakeData: 'fake_data'), config: FakeCacheConfig());
+
+    test('should be able to update data cache with prefs successfully', () async {
+      when(() => datasource.update<String>(dto)).thenAsyncVoid();
+
+      final response = await sut.update<String>(dto);
+
+      expect(response.isSuccess, isTrue);
+      expect(response.success, isA<Unit>());
+      verify(() => datasource.update<String>(dto)).called(1);
+    });
+
+    test('should NOT be able to update data cache when datasource fails', () async {
+      when(() => datasource.update<String>(dto)).thenThrow(FakeAutoCacheException());
+
+      final response = await sut.update<String>(dto);
+
+      expect(response.isError, isTrue);
+      expect(response.error, isA<AutoCacheException>());
+      verify(() => datasource.update<String>(dto)).called(1);
     });
   });
 
