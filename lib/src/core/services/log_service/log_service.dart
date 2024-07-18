83c7483770427b1ca98fa36557973c323ce9911b
Luiz Gasparetto <luizegasp@gmail.com>
2024-07-18 15:50:25 -0300

    refactor: log service & folder structure

--- lib/src/core/services/log_service/log_service.dart
+++ lib/src/core/services/log_service/log_service.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
+import '../../../../flutter_auto_cache.dart';
+
 final class LogService {
-  static void logException(String code, String message, StackTrace stackTrace) {
-    debugPrint('\x1B[31m[FlutterAutoCache] $code: $message\x1B[0m');
+  static void logException(AutoCacheException exception) {
+    if (!Platform.environment.containsKey('FLUTTER_TEST')) {
+      debugPrint('\x1B[31m[FlutterAutoCache] ${exception.code}: ${exception.message}\x1B[0m');
+    }
   }
 }
