8115a14b5617c1ff2564c3c60c3940453bd9d4ff
zanelladev <arturf.zanella@gmail.com>
2024-07-26 09:17:42 -0300

    fix: add widgets ensure initialized

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter_auto_cache/flutter_auto_cache.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_auto_cache/flutter_auto_cache.dart';
 
 import 'src/constants/cache_constants.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await AutoCacheInitializer.initialize();
 
   runApp(const AppWidget());
