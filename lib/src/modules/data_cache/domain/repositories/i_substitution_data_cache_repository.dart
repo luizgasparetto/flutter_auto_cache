095b888c7326b3bfd1db525d969242f76b2840d6
zanelladev <arturf.zanella@gmail.com>
2024-08-13 19:51:46 -0300

    docs: new result document

--- lib/src/modules/data_cache/domain/repositories/i_substitution_data_cache_repository.dart
+++ lib/src/modules/data_cache/domain/repositories/i_substitution_data_cache_repository.dart
@@ -9,23 +9,23 @@ part of 'i_data_cache_repository.dart';
 /// - Attempt to accommodate a given data cache entity, with optional recursive handling.
 ///
 /// Error handling is performed using the [Result] and [AsyncResult] types, which encapsulate
-/// the result or an [AutoCacheException] in case of failure, ensuring type safety and
+/// the result or an [AutoCacheError] in case of failure, ensuring type safety and
 /// robust error management.
 abstract interface class ISubstitutionDataCacheRepository {
   /// Retrieves all keys associated with a specific storage type.
   ///
   /// Returns:
-  /// - An [Result] containing an [AutoCacheException] on failure, or a list of strings representing the cache keys on success.
+  /// - An [Result] containing an [AutoCacheError] on failure, or a list of strings representing the cache keys on success.
   Result<List<String>> getKeys();
 
   /// Retrieves all cached entities.
   ///
   /// This method returns a comprehensive list of all entities currently stored in the cache. The result is
-  /// wrapped in an [Result] which contains an [AutoCacheException] in case of a failure, or a list of [DataCacheEntity] in case
+  /// wrapped in an [Result] which contains an [AutoCacheError] in case of a failure, or a list of [DataCacheEntity] in case
   /// of success. This allows for safe error handling and ensures type safety.
   ///
   /// Returns:
-  /// - An [Result] containing an [AutoCacheException] on failure, or a nullable [DataCacheEntity] on success.
+  /// - An [Result] containing an [AutoCacheError] on failure, or a nullable [DataCacheEntity] on success.
   Result<List<DataCacheEntity?>> getAll();
 
   /// Attempts to accommodate the given data cache entity.
@@ -34,6 +34,6 @@ abstract interface class ISubstitutionDataCacheRepository {
   /// it will recursively attempt to make space for the new data.
   ///
   /// Returns:
-  /// - An [AsyncResult] containing an [AutoCacheException] on failure, or a boolean indicating success.
+  /// - An [AsyncResult] containing an [AutoCacheError] on failure, or a boolean indicating success.
   AsyncResult<bool> accomodateCache<T extends Object>(DataCacheEntity<T> cache, {String? key});
 }
